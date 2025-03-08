cce2dbffb9417920d4ee2cd44ada683f3c3a540b
Lokendra S <slokendra2102@gmail.com>
2025-03-08 20:51:09 +0530

    Time: 1 ms (92.10%) | Memory: 41.7 MB (40.60%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -4,20 +4,14 @@ class Solution {
         int i=0, j=0;
         int minop = Integer.MAX_VALUE;
         int currop = 0;
-        int consec = 0;
 
         while(j < len) {
             if(blocks.charAt(j) == 'W') {
-                if(consec >= k) return 0;
                 currop++;
-                consec = 0;
             }
-            if(blocks.charAt(j) == 'B') consec++;
             if(j - i + 1 == k) {
-                // System.out.println("BEFORE :: " + minop + " ::: " + currop + " :: " + blocks.substring(i, j+1));
                 minop = Math.min(minop, currop);
                 currop = Math.max(0, blocks.charAt(i) == 'W' ? currop-1 : currop);
-                // System.out.println("AFTER :: " + minop + " ::: " + currop + " :: " + blocks.charAt(i) + " :: " + i + " :: " + j + "\n");
                 i++;
             }
             j++;
