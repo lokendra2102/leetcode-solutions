980c1c43051424a9652b6ef7e425daab9795f20a
Lokendra S <slokendra2102@gmail.com>
2025-03-08 20:53:03 +0530

    Time: 1 ms (92.10%) | Memory: 41.7 MB (26.70%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,20 +1,14 @@
 class Solution {
     public int minimumRecolors(String blocks, int k) {
         int len = blocks.length();
-        int i=0, j=0;
-        int minop = Integer.MAX_VALUE;
-        int currop = 0;
+        int i = 0, j = -1, minop = Integer.MAX_VALUE, currop = 0;
 
-        while(j < len) {
-            if(blocks.charAt(j) == 'W') {
-                currop++;
-            }
+        while(++j < len) {
+            if(blocks.charAt(j) == 'W') currop++;
             if(j - i + 1 == k) {
                 minop = Math.min(minop, currop);
-                currop = Math.max(0, blocks.charAt(i) == 'W' ? currop-1 : currop);
-                i++;
+                currop = Math.max(0, blocks.charAt(i++) == 'W' ? currop-1 : currop);
             }
-            j++;
         }
 
         return minop == Integer.MAX_VALUE ? 0 : minop;
