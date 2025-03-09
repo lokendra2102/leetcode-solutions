3d086bbb73383e9da7afa7281d476a23f00c4ee1
Lokendra S <slokendra2102@gmail.com>
2025-03-09 20:34:04 +0530

    Time: 3 ms (86.87%) | Memory: 56.3 MB (95.81%) - LeetSync

--- 3483-alternating-groups-ii/alternating-groups-ii.java
+++ 3483-alternating-groups-ii/alternating-groups-ii.java
@@ -3,26 +3,7 @@ class Solution {
         int len = colors.length;
         int[] rearr = new int[len + t -1];
 
-        int start = -1;
-        for(int i=0; i<len-1; i++) {
-            if(colors[i] == colors[i+1]) {
-                start = i+1;
-                break;
-            }
-        }
-
-        int k=0;
-        if(start != -1) {
-            for(int i=start; i<len; i++) {
-                rearr[k++] = colors[i];
-            }
-            for(int i=0; i<start; i++) {
-                rearr[k++] = colors[i];
-            }
-        }else{
-            System.arraycopy(colors, 0, rearr, 0, len);
-        }
-
+        System.arraycopy(colors, 0, rearr, 0, len);
         System.arraycopy(rearr, 0, rearr, len, t-1);
         int i=0, j=0;
         int count = 0;
